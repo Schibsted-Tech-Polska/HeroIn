80c2e8428aa0607b3c13cfcb32a7aa60069cd9f3
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2015-09-14 13:58:25 +0200

    config vars supported when creating a new app

--- test/hiaac_test.js
+++ test/hiaac_test.js
@@ -14,26 +14,37 @@ describe('hiaac', function () {
 
   it('should create simple heroku app', function (done) {
     var heroku_client = {
-      create_invocations: [],
+      app_spec: [],
+      config_vars_spec: [],
       apps: function () {
         return {
           create: function (app_spec) {
-            heroku_client.create_invocations.push(app_spec);
+            heroku_client.app_spec.push(app_spec);
             return Promise.resolve(app_spec);
+          },
+          configVars: function() {
+            return {
+              update: function(config_vars_spec) {
+                heroku_client.config_vars_spec.push(config_vars_spec);
+                return Promise.resolve(config_vars_spec);
+              }
+            }
           }
         }
       }
     };
+
     var configurator = hiaac(heroku_client);
     var simple_app_configuration = {
       name: 'sample_heroku_app',
-      'region:name': 'eu',
+      region: 'eu',
       config_vars: {
         NODE_ENV: "production"
       }
     };
     configurator(simple_app_configuration).then(function () {
-      assert.deepEqual(heroku_client.create_invocations, [{name: 'sample_heroku_app', 'region:name': 'eu'}]);
+      assert.deepEqual(heroku_client.app_spec, [{name: 'sample_heroku_app', region: 'eu'}]);
+      assert.deepEqual(heroku_client.config_vars_spec, [{NODE_ENV: "production"}]);
       done();
     }).catch(function(err) {
       done(err);
