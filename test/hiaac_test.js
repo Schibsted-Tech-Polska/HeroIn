e686c11afc82a40fa773f393e0da95fc7d00ccc9
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2015-09-14 11:34:24 +0200

    test for simple app creation

--- test/hiaac_test.js
+++ test/hiaac_test.js
@@ -1,23 +1,36 @@
-var chai        = require('chai'),
-    assert      = chai.assert,
-    hiaac       = require('../lib/hiaac');
+var chai = require('chai'),
+  assert = chai.assert,
+  hiaac = require('../lib/hiaac');
 
-describe('hiaac', function(){
-    it('should prompt you for an API key', function() {
-      try {
-        var configurator = hiaac();
-        assert.ok(false, 'should not allow empty token');
-      } catch(e) {
-        assert.equal(e.message, 'Please set env var for HEROKU_API_TOKEN');
+describe('hiaac', function () {
+  it('should prompt you for an API key', function () {
+    try {
+      var configurator = hiaac();
+      assert.ok(false, 'should not allow empty token');
+    } catch (e) {
+      assert.equal(e.message, 'Please set env var for HEROKU_API_TOKEN or specify a client library');
+    }
+  });
+
+  it('should create simple heroku app', function (done) {
+    var heroku_client = {
+      apps: function () {
+        return {
+          create: function (app_spec) {
+            return Promise.resolve(app_spec);
+          }
+        }
       }
+    };
+    var configurator = hiaac(heroku_client);
+    var simple_app_configuration = {
+      'name': 'sample_heroku_app',
+      'region:name': 'eu'
+    };
+    configurator(simple_app_configuration).then(function (app_created) {
+      assert.deepEqual(app_created, simple_app_configuration);
+      done();
     });
-
-    it('should create simple heroku app', function() {
-      var configurator = hiaac(process.env.HEROKU_API_TOKEN);
-      var simple_app_configuration = {
-        name: 'sample_heroku_app'
-      };
-      configurator(simple_app_configuration);
-    });
+  });
 
 });
