35b43263a3e322fd3d7f18194dac56cd657b7220
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2015-11-09 16:58:51 +0100

    Update simple.js

--- tutorial/simple.js
+++ tutorial/simple.js
@@ -1,3 +1,27 @@
-/**
- * Created by kwasniew on 05/10/15.
- */
+var heroin = require('heroin-js');
+
+var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+
+configurator.export('widget-farticle-saleposters').then(function(result) {
+	console.log(result);
+});
+
+configurator({ name: 'simple-widget',
+  region: 'eu',
+  maintenance: false,
+  stack: 'cedar-14',
+  config_vars: {},
+  addons: { logentries: { plan: 'logentries:le_tryit' } },
+  collaborators:
+   [ 'mateusz.kwasniewski@schibsted.pl' ],
+  features:
+   { 'runtime-dyno-metadata': { enabled: false },
+     'log-runtime-metrics': { enabled: false },
+     'http-session-affinity': { enabled: false },
+     preboot: { enabled: false },
+     'http-shard-header': { enabled: false },
+     'http-end-to-end-continue': { enabled: false },
+     'runtime-px-upgrade': { enabled: true } },
+  formation: [ { process: 'web', quantity: 1, size: 'Free' } ]
+  }
+);
