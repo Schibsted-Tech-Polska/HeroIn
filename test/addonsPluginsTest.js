1948a41facbe6ba88079e4075bdd8db59096e0a5
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-05-04 14:40:58 +0200

    smaller unit tests for addons config

---
+++ test/addonsPluginsTest.js
@@ -0,0 +1,33 @@
+var chai = require('chai'),
+  assert = chai.assert,
+  _ = require('lodash'),
+  addonsPluginsModule = require('../lib/addonsPlugins');
+
+// TODO: failing plugin configure, happy export, failing export
+// ignore field in heroku API call, pass env vars to librato addon, use field to make call to librato directly
+
+describe('Addons plugins', function () {
+  it('should pass correct addon config to plugin', function (done) {
+    var plugins = [{
+      librato: {
+        alerts: {
+          configure: function (config, configVars) {
+            return Promise.resolve(config);
+          }
+        }
+      }
+    }];
+    var addonsPlugins = addonsPluginsModule(plugins);
+    addonsPlugins.configure({
+        librato: {
+          plan: 'librato:development',
+          alerts: 'alerts_config_placeholder'
+        }
+      }
+    ).then(function (result) {
+        assert.equal(result, 'alerts_config_placeholder');
+        done();
+      }).catch(done);
+  });
+
+});
