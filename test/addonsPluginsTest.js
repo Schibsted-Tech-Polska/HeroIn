4b38e56a2d02a57fb0dad3c99c6c481251acfd44
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-05-05 11:21:03 +0200

    unhappy path tests

--- test/addonsPluginsTest.js
+++ test/addonsPluginsTest.js
@@ -51,4 +51,47 @@ describe('Addon plugin', function () {
     }).catch(done);
   });
 
+  it('should propagate configuration failure', function (done) {
+    var plugins = {
+      addonName: {
+        extension: {
+          configure: function (config, configVars) {
+            return Promise.reject('error');
+          }
+        }
+      }
+    };
+
+    var addonsPlugins = addonsPluginsModule(plugins);
+    var addons = {
+      addonName: {
+        plan: 'librato:development',
+        extension: 'alerts_config_placeholder'
+      }
+    };
+    addonsPlugins.configure(addons).catch(function (error) {
+      assert.equal(error, 'error');
+      done();
+    }).catch(done);
+  });
+
+  it('should propagate export failure', function (done) {
+    var plugins = {
+      addonName: {
+        extension: {
+          export: function () {
+            return Promise.reject('error');
+          }
+        }
+      }
+    };
+
+    var addonsPlugins = addonsPluginsModule(plugins);
+
+    addonsPlugins.export().catch(function (error) {
+      assert.equal(error, 'error');
+      done();
+    }).catch(done);
+  });
+
 });
