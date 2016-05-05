d0a606d7b9ea1c301ee78687c125d23455218127
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2016-05-05 14:07:48 +0200

    file with plugins usage tutorial

---
+++ tutorial/plugins.js
@@ -0,0 +1,37 @@
+//var heroin = require('heroin-js');
+var heroin = require('../lib/heroin');
+var util = require('util');
+
+var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+configurator.addPlugin({
+  logentries: {
+    alerts: {
+      configure: function (config, configVars) {
+        // make your API call here
+        console.log('Configuring plugin with config ', config, 'and additional config vars', configVars);
+        return Promise.resolve();
+      },
+      export: function () {
+        // make your API call here
+        return Promise.resolve({conf: 'alerts_config_placeholder'});
+      }
+    }
+  }
+});
+
+configurator({
+    name: 'simple-widget',
+    region: 'eu',
+    config_vars: {
+      ADDON_CONFIG_VAR: 'test'
+    },
+    addons: {logentries: {plan: 'logentries:le_tryit', alerts: {conf: 'alerts_config_placeholder'}}},
+    collaborators: ['mateusz.kwasniewski@schibsted.pl']
+  }
+);
+
+configurator.export('simple-widget').then(function (result) {
+  console.log(util.inspect(result, {depth: 3, colors: true}));
+});
+
+
